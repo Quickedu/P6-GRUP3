bd727378bd9e71a9bf13a750f0fe6975ef1cc502
CarlaCarmona <carlacarmona9@gmail.com>
2026-04-10 18:21:11 +0200

    migrations for type tables and dates, seeders for type tables

---
+++ database/migrations/2026_04_10_144554_create_dates_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dates', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('patient_id')
+                  ->constrained('patients')
+                  ->onDelete('cascade');
+            
+            $table->foreignId('worker_id')
+                  ->constrained('workers')
+                  ->onDelete('cascade');
+            
+            $table->foreignId('test_id')
+                  ->constrained('test_types')
+                  ->onDelete('cascade');
+
+            $table->date('date_time');
+            
+            $table->integer('time');
+
+            $table->enum ('estat',['programada', 'cancel·lada', 'realitzada']);
+
+            $table->enum ('urgencia',['no urgent', 'preferent', 'urgent']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dates');
+    }
+};
---
+++ database/seeders/TestTypeSeeder.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TestTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('test_types')->insert([
+            [
+                'name' => 'Radiografia Simple',
+                'time' => 15
+            ],
+            [
+                'name' => 'Radiografia Tòrax',
+                'time' => 5
+            ],
+            [
+                'name' => 'Radiografia Pelvis',
+                'time' => 8
+            ],
+            [
+                'name' => 'Resonància Magnètica',
+                'time' => 60
+            ],
+            [
+                'name' => 'Resonància Magnètica Lumbar',
+                'time' => 20
+            ],
+            [
+                'name' => 'Resonància Magnètica Cranial',
+                'time' => 25
+            ],
+            [
+                'name' => 'Ecografia',
+                'time' => 15
+            ],
+            [
+                'name' => 'Ecografia Renal',
+                'time' => 10
+            ],
+            [
+                'name' => 'Ecografia Cardíaca',
+                'time' => 10
+            ],
+            [
+                'name' => 'Ecografia Cardíaca',
+                'time' => 15
+            ], 
+            [
+                'name' => 'Mamografia',
+                'time' => 10
+            ],
+            [
+                'name' => 'Mamografia Craneocaudal',
+                'time' => 5
+            ],
+            [
+                'name' => 'Mamografia Obliqua',
+                'time' => 5
+            ],
+            [
+                'name' => 'Tomografía axial computada (TAC)',
+                'time' => 30
+            ],
+            [
+                'name' => 'Tomografía axial computada (TAC) Abdomen',
+                'time' => 10
+            ],
+            [
+                'name' => 'Tomografía axial computada (TAC) Crani',
+                'time' => 10
+            ],
+        ]);
+    }
+}
