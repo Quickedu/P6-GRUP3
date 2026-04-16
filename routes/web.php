1ee6e62fa6587c0108dcae1c97ed087648d67689
Quickedu <eduard.martinez.teixidor@gmail.com>
2026-04-16 15:58:21 +0200

    Added variable to dashboard to get user

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 Route::inertia('/', 'Welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+    Route::get('/dashboard', function (){
+        return Inertia::render('Dashboard', [
+            'role' => Auth::user()->role
+        ]);
+    })->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
