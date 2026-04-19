bf41c5b9e97c4dc95d867e8a2933b4ef7996275b
Sara Alaoui <saraala7@gmail.com>
2026-04-19 19:37:44 +0200

    .

--- app/Http/Middleware/isPatient.php
+++ app/Http/Middleware/isPatient.php
@@ -5,16 +5,22 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class isPatient
 {
     /**
      * Handle an incoming request.
+     * Los pacientes son usuarios autenticados SIN role.
      *
      * @param  Closure(Request): (Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (Auth::check() && !Auth::user()->role) {
+            return $next($request);
+        }
+
+        return redirect('/');
     }
 }
--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 import { BookOpen, FolderGit2, LayoutGrid, CalendarPlus } from 'lucide-vue-next';
 import AppLogo from '@/components/AppLogo.vue';
 import NavFooter from '@/components/NavFooter.vue';
@@ -17,6 +18,15 @@ import {
 import { dashboard } from '@/routes';
 import { novaCita } from '@/routes';
 import type { NavItem } from '@/types';
+import { Microscope, Users, BookUser, History, Cross } from 'lucide-vue-next';
+
+const page = usePage();
+
+const user = computed(() => page.props.auth?.user);
+
+const isPatient = computed(() => !user.value?.role);
+const isAdmin = computed(() => user.value?.role === 'admin');
+const isWorker = computed(() => user.value?.role === 'doctor' || user.value?.role === 'secretary');
 
 const mainNavItems: NavItem[] = [
     {
@@ -24,12 +34,60 @@ const mainNavItems: NavItem[] = [
         href: dashboard(),
         icon: LayoutGrid,
     },
+];
+
+const AdminNavItems: NavItem[] = [
+    {
+        title: 'Proves diagnostic',
+        href: '/',
+        icon: Microscope,
+    },
+    {
+        title: 'Gestió de personal',
+        href: '/',
+        icon: BookUser,
+    }
+]
+
+// const mainSecretaryNavItems: NavItem[] = [
+
+// ]
+
+// const mainDoctorNavItems: NavItem[] = [
+    
+// ]
+
+const PatientNavItems: NavItem[] = [
+    {
+        title: 'Historial de cites',
+        href: '/',
+        icon: History,
+    }
+]
+
+const WorkerNavItems: NavItem[] = [
     {
         title: 'Nova Cita',
         href: novaCita(),
         icon: CalendarPlus,
+    },
+    {
+        title: 'Pacients',
+        href: '/',
+        icon: Users,
+    },
+    {
+        title: 'Necessitats dels pacients',
+        href: '/',
+        icon: Cross,
+    },
+    {
+        title: 'Historial de cites',
+        href: '/',
+        icon: History,
     }
-];
+]
+
 
 const footerNavItems: NavItem[] = [
     {
@@ -61,6 +119,9 @@ const footerNavItems: NavItem[] = [
 
         <SidebarContent>
             <NavMain :items="mainNavItems" />
+            <NavMain label="Administració" v-if="isAdmin" :items="AdminNavItems"/>
+            <NavMain label="Treballadors" v-if="isWorker" :items="WorkerNavItems"/>
+            <NavMain label="Pacients" v-if="isPatient" :items="PatientNavItems"/>
         </SidebarContent>
 
         <SidebarFooter>
