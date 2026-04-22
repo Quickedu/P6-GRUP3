79c48a0e7c1b03292d9d7f200fbe5b6c538f7452
CarlaCarmona <carlacarmona9@gmail.com>
2026-04-22 16:03:22 +0200

    began doing the secretary dash

--- resources/js/components/Dashboard/SecretaryDashboard.vue
+++ resources/js/components/Dashboard/SecretaryDashboard.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+  import { Head, Link, usePage } from '@inertiajs/vue3'
+  import { patientDashboard } from '@/routes';
+  import { computed } from 'vue';
+  import { Calendar, Clock, AlertCircle } from 'lucide-vue-next';
+</script>
+
 <template>
   Dashboard Secretari
 </template>
--- resources/js/pages/patientDashboard.vue
+++ resources/js/pages/patientDashboard.vue
@@ -1,8 +1,8 @@
-<script setup lang="ts">
-import { Head, Link, usePage } from '@inertiajs/vue3'
-import { patientDashboard } from '@/routes';
-import { computed } from 'vue';
-import { Calendar, Clock, AlertCircle } from 'lucide-vue-next';
+    <script setup lang="ts">
+    import { Head, Link, usePage } from '@inertiajs/vue3'
+    import { patientDashboard } from '@/routes';
+    import { computed } from 'vue';
+    import { Calendar, Clock, AlertCircle } from 'lucide-vue-next';
 
 interface DateRecord {
     id: number;
