28ab692d6f3c3834e6dcfb6d90d5faf35d7fd6ab
jponcelas <jponcelas@cendrassos.net>
2026-05-13 18:35:45 +0200

    add logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,16 +11,16 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/logo.png" sizes="any">
+        <link rel="icon" href="/logo.png" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/logo.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @vite(['resources/css/app.css', 'resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'PMF') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
