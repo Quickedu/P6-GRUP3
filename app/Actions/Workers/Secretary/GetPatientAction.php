0a69e485618c8d032fe976b503e63117d404d869
Quickedu <eduard.martinez.teixidor@gmail.com>
2026-05-13 16:25:27 +0200

    Get patient action created

---
+++ app/Actions/Workers/Secretary/GetPatientAction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Actions\Workers\Secretary;
+
+use App\Models\Patient;
+
+class GetPatientAction
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function handle(string $nts): array
+    {
+        $patient = Patient::query()->where('nts', $nts)->first();
+
+        if (! $patient) {
+            return [
+                'status' => 'error',
+                'message' => 'Pacient no trobat',
+                'available' => false,
+                'data' => [],
+            ];
+        }
+
+        return [
+            'status' => 'success',
+            'message' => 'Pacient trobat',
+            'available' => true,
+            'data' => $patient,
+        ];
+    }
+}
